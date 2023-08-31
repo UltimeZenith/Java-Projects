1fb472caa62b0f360d9115e6bba666b3a49309aa
UltimeZenith <142943740+UltimeZenith@users.noreply.github.com>
2023-08-31 17:52:00 -0500

    Add files via upload

---
+++ The-Beginning/Archivos java/Introduction_Java/Stars.java
@@ -0,0 +1,15 @@
+class Stars{
+	public static void main(String[] args){
+		System.out.println("Hola!!, ¿te gustan las estrellas? \t Si es así entonces mira esta estrella!\n\n");
+		System.out.println("         *");
+		System.out.println("        * *");
+		System.out.println("       *   *");
+		System.out.println("* * * *     * * * *");
+		System.out.println(" *               *");
+		System.out.println("  *             *");
+		System.out.println("   *     *     *");
+		System.out.println("  *   *     *   *");
+		System.out.println(" *  *         *  *");
+		System.out.println("*                 *");
+	}
+}
---
+++ The-Beginning/Archivos java/Introduction_Java/additionSubstraction.java
@@ -0,0 +1,11 @@
+public class additionSubstraction{
+	public static void main(String[] args){
+		int Papas = 51;
+		int Arepas = 72;
+		int PapasMasArepas = Papas + Arepas;
+		
+		System.out.println("La suma de " + Papas + " papas más la suma de " + Arepas + " arepas es igual a: "+ PapasMasArepas);
+		int Comer = PapasMasArepas - 56;
+		System.out.println("Vaya tenias hambre, te comiste 28 papas y 28 arepas, en total te quedaron: "+ Comer);
+	}
+}
