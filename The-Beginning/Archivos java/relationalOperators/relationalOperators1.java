471ad2fd2097df793acf3b6cc534289078a84e27
UltimeZenith <142943740+UltimeZenith@users.noreply.github.com>
2023-09-06 13:40:32 -0500

    Add files via upload

---
+++ The-Beginning/Archivos java/relationalOperators/relationalOperators1.java
@@ -0,0 +1,10 @@
+public class relationalOperators1{
+	public static void main(String[] args){
+		double valorAdquirido = 128.5;
+		double valorEvaluar = 180;
+		double valorAgregar = 60;
+		double valorSumar = valorAdquirido + valorAgregar;
+		System.out.println(valorAdquirido > valorEvaluar);
+		System.out.println(valorEvaluar < valorSumar);
+	}
+}
