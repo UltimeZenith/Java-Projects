47f94a5bab4ffc83c974c17a3f822b0da649bfd6
UltimeZenith <142943740+UltimeZenith@users.noreply.github.com>
2023-09-19 23:04:21 -0500

    Update CheckingAccount.java

--- The-Beginning/Archivos java/AccessEncapsulationScope/Bank/CheckingAccount.java
+++ The-Beginning/Archivos java/AccessEncapsulationScope/Bank/CheckingAccount.java
@@ -1,9 +1,25 @@
 public class CheckingAccount{
-	public String name;
-	public int balance;
+	private String name;
+	private int balance;
+	private String id;
+	private double interestRate;
 
 	public CheckingAccount(String inputName, int inputBalance){
-		name = inputName;
-		balance = inputBalance;
+		this.name = inputName;
+		this.balance = inputBalance;
+		this.id = inputId;
+		this.interestRate = 0.02;
+	}
+
+	public int getBalance(){
+		return this.balance;
+	}
+
+	public void setBalance(int newBalance){
+		this.balance = newBalance;
+	}
+
+	public double getMonthlyInterest(){
+		return this.interestRate * this.balance;
 	}
 }
