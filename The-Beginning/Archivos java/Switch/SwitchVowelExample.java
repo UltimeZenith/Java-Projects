0ec7b7b71c63a52ae302e0dbbd2ffce5d9ffa54e
UltimeZenith <142943740+UltimeZenith@users.noreply.github.com>
2023-09-23 17:42:09 -0500

    Add files via upload

---
+++ The-Beginning/Archivos java/Switch/SwitchVowelExample.java
@@ -0,0 +1,49 @@
+public class SwitchVowelExample {    
+	public static void main(String[] args) {    
+		char ch='O';    
+		switch(ch){
+			case 'a':   
+			System.out.println("Vocal");  
+			break;
+
+			case 'e':
+			System.out.println("Vocal");  
+			break;
+
+			case 'i':
+			System.out.println("Vocal");  
+			break;
+
+			case 'o':
+			System.out.println("Vocal");  
+			break;
+
+			case 'u':   
+			System.out.println("Vocal");  
+			break;
+
+			case 'A':   
+			System.out.println("Vocal");  
+			break;
+
+			case 'E':
+			System.out.println("Vocal");  
+			break;
+
+			case 'I':   
+			System.out.println("Vocal");  
+			break;
+
+			case 'O':
+			System.out.println("Vocal");  
+			break;
+
+			case 'U':
+			System.out.println("Vocal");  
+			break;
+
+			default:   
+			System.out.println("Consonante");  
+		}
+	}
+} 
