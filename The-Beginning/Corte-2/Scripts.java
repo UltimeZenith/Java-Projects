49b4b946527f93b6a63bcda093ceb0f6f760124e
UltimeZenith <142943740+UltimeZenith@users.noreply.github.com>
2023-11-01 13:59:06 -0500

    Add files via upload

---
+++ The-Beginning/Corte-2/Scripts.java
@@ -0,0 +1,23 @@
+class Circle{
+	double pi = 3.1416;
+	public double area(double radius){
+		double C = pi*radius*radius;
+		return C;
+	}
+}
+
+public class Scripts
+{
+
+	public static void main(String[] args)
+	{
+		Circle Circulo = new Circle();
+		double pizza_area = Circulo.area(12/2);
+		double teaching_table_area = Circulo.area(36/2);
+		double round_room_area = Circulo.area(11460/2);
+		System.out.println(pizza_area);
+		System.out.println(teaching_table_area);
+		System.out.println(round_room_area);
+	}
+	
+}
